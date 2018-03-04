40aaa4c05f82f8cd8793b65685d9bc2986fcdbd7
Hugoefr <hugoefr94@gmail.com>
2018-03-04 13:56:48 -0600

    Video agregado al playlist

--- Respaldo/js/functions.js
+++ Respaldo/js/functions.js
@@ -20,18 +20,16 @@ function cambiarVideo(seleccion) {
 
 function dragover_handler(ev) {
     ev.preventDefault();
-    ev.dataTransfer.dropEffect = "copy"; //aqui es para selecciona la accion, si quieres ue solo lo mueva o lo copie
+    ev.dataTransfer.dropEffect = "copy";
 }
 function drop_handler(ev) {
     ev.preventDefault();
-    // Get the id of the target and add the moved element to the target's DOM
-    var data = ev.dataTransfer.files;//asignas el vector de archivos a una variable
-    //var listado="";
-    //localStorage.setItem(listado);
-    var video=document.getElementById("videos");//agarras la etiqueta video para poder editarla
+    var data = ev.dataTransfer.files;
 
-    video.src=window.URL.createObjectURL(data[0]);//este comando te convierte a url el archivo, es necesario para poder reproducir el archivo en el cource
-    video.onload = function() {//ponle esta funcon ahuevo, ya que esto hara que se reproduzca bien el video  
+    var video=document.getElementById("videos");
+
+    video.src=window.URL.createObjectURL(data[0])
+    video.onload = function() {
         window.URL.revokeObjectURL(this.src);
       }
     video.load();
--- js/functions.js
+++ js/functions.js
@@ -13,27 +13,35 @@ function cambiarVideo(seleccion) {
         video.src = "Overwatch Animated Short  “Dragons”.mp4";
     } else if (seleccion == "Hero") {
         video.src = "Overwatch Animated Short  “Hero”.mp4";
-    } else {
+    } else if(seleccion == "The Last Bastion"){
         video.src = "Overwatch Animated Short  The Last Bastion.mp4";
+    }else{
+        video.src=seleccion;
     }
 }
-
+/*function newPlaylist() {
+    var lista = document.getElementById("listaVideos");
+    var option = document.createElement("OPTION");
+    lista.appendChild(option);
+}*/
 function dragover_handler(ev) {
     ev.preventDefault();
-    ev.dataTransfer.dropEffect = "copy"; //aqui es para selecciona la accion, si quieres ue solo lo mueva o lo copie
+    ev.dataTransfer.dropEffect = "copy";
 }
 function drop_handler(ev) {
     ev.preventDefault();
-    // Get the id of the target and add the moved element to the target's DOM
-    var data = ev.dataTransfer.files;//asignas el vector de archivos a una variable
-    //var listado="";
-    //localStorage.setItem(listado);
-    var video=document.getElementById("videos");//agarras la etiqueta video para poder editarla
+    var data = ev.dataTransfer.files;
+    var video = document.getElementById("videos");
 
-    video.src=window.URL.createObjectURL(data[0]);//este comando te convierte a url el archivo, es necesario para poder reproducir el archivo en el cource
-    video.onload = function() {//ponle esta funcon ahuevo, ya que esto hara que se reproduzca bien el video  
+    video.src = window.URL.createObjectURL(data[0]);
+    video.onload = function () {
         window.URL.revokeObjectURL(this.src);
-      }
-    video.load();
     }
+    var lista = document.getElementById("listaVideos");
+    var option = document.createElement("OPTION");
+    option.textContent=data[0].name;
+    option.value=window.URL.createObjectURL(data[0]);
+    lista.appendChild(option);
+    video.load();
+}
 
